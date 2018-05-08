30f84e143b6e5303f05a28a667cd3a62980045fa
Elysha Spector <elyshaspector@gmail.com>
2018-05-08 11:13:46 -0400

    Update names
    Edited names and comments to be clear

--- app/src/main/java/com/example/android/meowcount/MainActivity.java
+++ app/src/main/java/com/example/android/meowcount/MainActivity.java
@@ -22,21 +22,20 @@ public class MainActivity extends AppCompatActivity {
         displayForAlice(scoreAlice);
     }
 
-    /****** SCORE COUNT FOR CAT1 ******/
-
-    /***Adding 1 point to Cat1 score count***/
+    /****** SCORE COUNT FOR KYO (CAT 1) ******/
+    /***Adding 1 point to Cat 1 score count***/
     public void addMeowForKyo(View view) {
         scoreKyo = scoreKyo + 1;
         displayForKyo(scoreKyo);
     }
 
-    /***Adding 3 points to Cat1 score count***/
+    /***Adding 3 points to Cat 1 score count***/
     public void addPurrForKyo(View view) {
         scoreKyo = scoreKyo + 3;
         displayForKyo(scoreKyo);
     }
 
-    /***Subtracting 2 points from Cat1 score count***/
+    /***Subtracting 2 points from Cat 1 score count***/
     public void pointsReducedForKyo(View view) {
         scoreKyo = scoreKyo - 2;
         /***NOTE: Count cannot go below 0***/
@@ -45,21 +44,20 @@ public class MainActivity extends AppCompatActivity {
         } displayForKyo(scoreKyo);
     }
 
-    /****** SCORE COUNT FOR CAT2 ******/
-
-    /***Adding 1 point to Cat2 score count***/
+    /****** SCORE COUNT FOR ALICE (CAT 2) ******/
+    /***Adding 1 point to Cat 2 score count***/
     public void addMeowForAlice(View view) {
         scoreAlice = scoreAlice + 1;
         displayForAlice(scoreAlice);
     }
 
-    /***Adding 3 points to Cat2 score count***/
+    /***Adding 3 points to Cat 2 score count***/
     public void addPurrForAlice(View view) {
         scoreAlice = scoreAlice + 3;
         displayForAlice(scoreAlice);
     }
 
-    /***Subtracting 2 points from Cat2 score count***/
+    /***Subtracting 2 points from Cat 2 score count***/
     public void pointsReducedForAlice(View view) {
         scoreAlice = scoreAlice - 2;
         /***NOTE: Count cannot go below 0***/
@@ -68,8 +66,7 @@ public class MainActivity extends AppCompatActivity {
         } displayForAlice(scoreAlice);
     }
 
-
-    /***Resetting score count for CAT1 & CAT2 back to 0 points.***/
+    /***Resetting score count for CAT 1 & CAT 2 back to 0 points.***/
     public void resetScore(View view) {
         scoreKyo = 0;
         scoreAlice = 0;
@@ -77,13 +74,13 @@ public class MainActivity extends AppCompatActivity {
         displayForAlice(scoreAlice);
     }
 
-    /*** Displays the given score for CAT1.***/
+    /*** Displays the given score for CAT 1.***/
     public void displayForKyo(int score) {
         TextView scoreView = (TextView) findViewById(R.id.kyo_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    /*** Displays the given score for CAT2.***/
+    /*** Displays the given score for CAT 2.***/
     public void displayForAlice(int score) {
         TextView scoreView = (TextView) findViewById(R.id.alice_score);
         scoreView.setText(String.valueOf(score));
